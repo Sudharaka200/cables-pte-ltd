e2f4a150dd40892889f258bbc7d85b70b43514bd
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-07-15 03:43:50 +0530

    Project Structure Done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function showSection(sectionNumber) {
+    document.querySelectorAll('.product-section').forEach(section => {
+        section.classList.remove('active');
+    });
+
+    document.getElementById(`section${sectionNumber}`).classList.add('active');
+}
